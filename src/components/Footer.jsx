2f4dfefe3f38c7529244f354451b3abb3c6ff846
KenenthAtchon <kenneth.atchon@hotmail.com>
2024-06-02 13:35:52 -0500

    Completed most of the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,60 +3,64 @@ import { prognostic } from "../assets";
 import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      <div className="flex-[1] flex flex-col justify-start mr-10">
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col bg-[#092689]`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-14 `}>
+      <div className=" flex flex-col justify-start ">
         <img
           src={prognostic}
           alt="hoobank"
-          className="w-[266px] h-[172.14px] object-contain"
+          className="w-[200px] h-[130px] object-contain"
         />
-        <p className={`${styles.paragraph} mt-4 max-w-[500px]`}>
-          Revolutionalizing healthcare 
-        </p>
       </div>
 
-      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+      <div className="flex-[1.5] w-full flex flex-row justify-between md:mt-0 mt-10 pt-8">
         {footerLinks.map((footerlink) => (
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-              {footerlink.title}
+          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[200px] max-w-[700px]`}>
+            <h4 className="font-poppins font-medium text-[20px] leading-[27px] text-[#6C7A91] text-center">
+             <b>{footerlink.title}</b> 
             </h4>
             <ul className="list-none mt-4">
               {footerlink.links.map((link, index) => (
-                <li
-                  key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
-                >
-                  {link.name}
-                </li>
+                footerlink.title === "PRIVACY POLICY" ? (
+                  <li
+                    key={link.name}
+                    className={`font-poppins font-normal text-[20px] leading-[24px] text-[#6C7A91] cursor-default text-center ${
+                      index !== footerlink.links.length - 1 ? "mb-4" : "mr-6"
+                    }`}
+                  >
+                    {link.name}
+                  </li>
+                ) : (
+                  <li
+                    key={link.name}
+                    className={`font-poppins font-normal text-[20px] leading-[24px] text-[#6C7A91] hover:text-secondary cursor-pointer text-center ${
+                      index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                    }`}
+                  >
+                    {link.name}
+                  </li>
+                )
               ))}
             </ul>
           </div>
         ))}
       </div>
+
     </div>
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2024 POG AI. All Rights Reserved.
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6  bg-[#4F5D75] pb-5">
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white pl-12 ">
+         Ⓒ 2024 POG AI. All Rights Reserved.
       </p>
 
-      <div className="flex flex-row md:mt-0 mt-6">
-        {socialMedia.map((social, index) => (
-          <img
-            key={social.id}
-            src={social.icon}
-            alt={social.id}
-            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-            }`}
-            onClick={() => window.open(social.link)}
-          />
-        ))}
-      </div>
+      <div className="flex flex-row md:mt-0 mt-6 space-x-8 pr-8">
+        <a href="#" className="text-white text-[15px]">Privacy Policy</a>
+        <a href="#" className="text-white text-[15px]">Report a Problem</a>
+        <a href="#" className="text-white text-[15px]">Security</a>
+        <a href="#" className="text-white text-[15px]">Terms of Use</a>
+        <a href="#" className="text-white text-[15px]">Cookies Settings</a>
+    </div>
+
     </div>
   </section>
 );
